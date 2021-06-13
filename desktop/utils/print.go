6e68fed3ffd9f8b9e448e7a7cf5fb660a20fd268
Jeremiah <jmiller.v@gmail.com>
2021-06-13 12:43:42 -0600

    print: add util for printing interfaces

--- desktop/utils/print.go
+++ desktop/utils/print.go
@@ -1,1 +1,14 @@
 package utils
+
+import (
+	"encoding/json"
+	"fmt"
+)
+
+func InterfaceToString(i interface{}) string {
+	structBytes, err := json.Marshal(i)
+	if err != nil {
+		fmt.Println(err)
+	}
+	return string(structBytes)
+}
