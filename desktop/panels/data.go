c5e5ffea98d30e0ce1e563935d3d1c0a9f81be03
Jeremiah <jmiller.v@gmail.com>
2021-06-17 22:57:47 -0600

    panels: implement manage layout

--- desktop/panels/data.go
+++ desktop/panels/data.go
@@ -3,18 +3,18 @@ package panels
 import "fyne.io/fyne/v2"
 
 type Panel struct {
-	Title, Intro string
+	Title string
 	View func(w fyne.Window) fyne.CanvasObject
 }
 
 var (
 	// Panels defines metadata for each panel
 	Panels = map[string]Panel{
-		"home": {"Shortcast", "", homeScreen},
-		"getting started": {"Getting Started", "Help for a successful experience", gettigStartedScreen},
-		"configuration":{"Configuration", "Set up your Shortcast", configScreen},
-		"documentation": {"Documentation", "Guides and Best Practices", documentationScreen},
-		"manage": {"Manage", "Manage your Shortcast", manageScreen},
+		"home": {"Shortcast", homeScreen},
+		"getting started": {"Getting Started", gettigStartedScreen},
+		"configuration":{"Configuration", configScreen},
+		"documentation": {"Documentation",  documentationScreen},
+		"manage": {"Manage", manageScreen},
 	}
 
 	// PanelIndex defines how the panels are laid out in the index tree
--- desktop/panels/manage.go
+++ desktop/panels/manage.go
@@ -3,10 +3,33 @@ package panels
 import (
 	"fyne.io/fyne/v2"
 	"fyne.io/fyne/v2/container"
+	"fyne.io/fyne/v2/layout"
+	"fyne.io/fyne/v2/theme"
+	"fyne.io/fyne/v2/widget"
+	"github.com/jmillerv/shortcast/backend"
+	"github.com/jmillerv/shortcast/backend/posts"
 )
 
+
+
+
 func manageScreen(_ fyne.Window) fyne.CanvasObject {
-	return container.NewHBox()
+	startCastButton := widget.NewButtonWithIcon("start cast", theme.MediaPlayIcon(), backend.StartShortcast)
+	stopCastButton := widget.NewButtonWithIcon("stop cast", theme.MediaStopIcon(), backend.StopShortcast)
+	castControls := container.NewGridWithColumns(2, stopCastButton, startCastButton)
+	newPostButton := widget.NewButtonWithIcon("New", theme.ContentAddIcon(), posts.Add)
+	postSidePanel := container.NewGridWithRows(1, newPostButton)
+	newThemeButton := widget.NewButtonWithIcon("Upload", theme.UploadIcon(), posts.Add)
+	themeSidePanel := container.NewGridWithRows(1, newThemeButton)
+
+	postTabContent := container.New(layout.NewFormLayout(), postSidePanel, widget.NewLabel("list of posts with edit/remove buttons here"))
+	themeTabContent := container.New(layout.NewFormLayout(), themeSidePanel, widget.NewLabel("list of themes with edit remove"))
+
+	tabs := container.NewAppTabs(
+		container.NewTabItemWithIcon("Posts", theme.DocumentIcon(), postTabContent),
+		container.NewTabItemWithIcon("Themes", theme.ColorPaletteIcon(), themeTabContent),
+	)
+	return container.NewBorder(nil, castControls, tabs, nil)
 }
 
 
