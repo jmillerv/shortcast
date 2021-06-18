6dfe25ee0045bf06622bd2f0d7bae0492d795c37
Jeremiah <jmiller.v@gmail.com>
2021-06-17 22:35:41 -0600

    add print statements to start/stop

--- backend/run.go
+++ backend/run.go
@@ -1,9 +1,13 @@
 package backend
 
-func StartShortcast() error {
-	panic("implement me")
+import "fmt"
+
+func StartShortcast() {
+	// variadic function to pass commands maybe set theme from the command line?
+	// desktop can just use the yaml settings
+	fmt.Println("starting shortcast")
 }
 
-func StopShortcast() error {
-	panic("implement me")
+func StopShortcast() {
+	fmt.Println("stopping shortcast")
 }
