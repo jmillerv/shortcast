e14e42f4e73921137be540616e24660dabf1e40b
Jeremiah <jmiller.v@gmail.com>
2021-06-22 22:57:59 -0600

    format/format: implement skeleton functions

---
+++ editor/format/format.go
@@ -0,0 +1,48 @@
+package format
+
+import "fmt"
+
+func AddImage(){
+	fmt.Println("add image")
+}
+
+func Bold() {
+	fmt.Println("bold")
+}
+
+func Code() {
+	fmt.Println("code")
+}
+
+func Heading() {
+	fmt.Println("heading")
+}
+
+func Italic() {
+	fmt.Println("italic")
+}
+
+func Link() {
+	fmt.Println("link")
+}
+
+func List() {
+	fmt.Println("list")
+}
+
+func NumberedList() {
+	fmt.Println("numbered list")
+}
+
+
+func Strikethrough() {
+	fmt.Println("strikethrough")
+}
+
+func Table(){
+	fmt.Println("table")
+}
+
+func TaskList() {
+	fmt.Println("Task list")
+}
